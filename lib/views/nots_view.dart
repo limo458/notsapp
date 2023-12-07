d586f0dbc90fe94e8b89844d11122de699181a18
Limo <limo54281@gmail.com>
2023-12-07 03:27:14 +0200

    chanage theme and create nots view

---
+++ lib/views/nots_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotsView extends StatelessWidget {
+  const NotsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
