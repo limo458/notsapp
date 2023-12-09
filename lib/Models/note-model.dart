83bacadfe0cda8becc1cb96053897979695ab0be
Limo <limo54281@gmail.com>
2023-12-09 04:57:22 +0200

    add hive

---
+++ lib/Models/note-model.dart
@@ -0,0 +1,12 @@
+class NoteMoldel {
+  final String titel;
+  final String supTitel;
+  final String date;
+  final int color;
+
+  NoteMoldel(
+      {required this.titel,
+      required this.supTitel,
+      required this.date,
+      required this.color});
+}
